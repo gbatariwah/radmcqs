a0374983348faccae033334855ca29eb44101257
batariwah <batariwahg@gmail.com>
2023-07-16 21:08:12 +0000

    add scroll-to-top function

---
+++ components/ScrollToTop.vue
@@ -0,0 +1,38 @@
+<template>
+  <!--  <button-->
+  <!--    v-show="visible"-->
+  <!--    @click="scrollUp"-->
+  <!--    class="btn btn-circle outline-none fixed bottom-8 right-8 z-50 opacity-80 hover:opacity-100"-->
+  <!--  >-->
+  <!--    <i class="ph-duotone ph-arrow-u-left-up text-[28px]"></i>-->
+  <!--  </button>-->
+  <UButton
+    v-show="visible"
+    @click="scrollUp"
+    color="sky"
+    icon="i-ic-twotone-keyboard-double-arrow-up"
+    class="fixed bottom-14 right-8 z-50 ring ring-sky-500"
+    variant="soft"
+    :ui="{ rounded: 'rounded-full' }"
+  />
+</template>
+
+<script setup>
+import { useEventListener } from "@vueuse/core";
+
+const visible = ref(false);
+
+onMounted(() => {
+  useEventListener(document, "scroll", (e) => {
+    visible.value = window.scrollY > 150;
+  });
+});
+
+const scrollUp = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+</script>
